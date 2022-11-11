2e250aae8d82d611f8b74ddd1bb25a612c2d2479
martin-costa <costa.martin@outlook.com>
2022-11-11 20:50:18 +0000

    started data loading

--- Test.java
+++ Test.java
@@ -1,10 +1,52 @@
 import java.util.*;
+import java.io.*;
+
+import java.nio.ByteBuffer;
 
 public class Test {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
     DynamicMP coreset = new DynamicMP(10, new LpNorm(1), 2f, 0.5f, 0.01f);
+
+    // 'song' input path
+    String path = "../data/kddcup";
+
+    Scanner scanner = new Scanner(new File(path));
+
+    int n = scanner.nextInt();
+    int size = scanner.nextInt();
+
+    float[] feature = new float[size];
+
+    for (int i = 0; i < size; i++) {
+      feature[i] = scanner.nextFloat();
+    }
+
+    System.out.println(n);
+    System.out.println(size);
+
+    System.out.println(Arrays.toString(feature));
+    System.out.println(feature[25]);
+
+
+
+    // int count = (stream.read() << 24) | (stream.read() << 16) | (stream.read() << 8) | (stream.read());
+    //
+    // System.out.println(count);
+    //
+    //
+    // byte[] b = new byte[4];
+    // stream.read(b);
+    //
+    // System.out.println(ByteBuffer.wrap(b).getInt());
+    //
+    //
+    // String line = scanner.nextLine();
+    //
+    // for (int i = 0; i < 10; i++) {
+    //   System.out.println(scanner.nextLine());
+    // }
 
   }
 
--- UpdateStreamGenerator.java
+++ UpdateStreamGenerator.java
@@ -1,10 +1,42 @@
+import java.util.*;
+import java.io.*;
+
 // generates a stream of m updates given data path
 
-class UpdateStreamGenerator {
+abstract class UpdateStreamGenerator {
 
-  public UpdateStreamGenerator(String path, int m) {
+  // returns the data point in update i
+  public abstract float[] point(int i);
 
+  // returns the (unique) key corresponding to the data point in update i
+  public abstract int key(int i);
 
+  // returns whether update i is an insertion (true) / deletion (false)
+  public abstract boolean updateType(int i);
+
+}
+
+// generates update sequences in the sliding window model
+class SlidingWindow extends UpdateStreamGenerator {
+
+  // number of points to insert and delete
+  private int n;
+
+  // the length of the sliding window
+  private int windowLength;
+
+  // the data points
+  private float[][] data;
+
+  // stores the dimension of the data
+  private int d;
+
+  SlidingWindow(int n, int windowLength, String path) {
+
+    this.n = n;
+    this.windowLength = windowLength;
+
+    this.loadData(path);
   }
 
   // returns the data point in update i
@@ -20,6 +52,24 @@ class UpdateStreamGenerator {
   // returns whether update i is an insertion (true) / deletion (false)
   public boolean updateType(int i) {
 
+  }
+
+  // loads the data points
+  private void loadData(String path) throws IOException {
+
+    // create scanner to read from file
+    Scanner scanner = new Scanner(new File(path));
+
+    this.n = Math.min(scanner.nextInt(), this.n);
+    this.d = scanner.nextInt();
+
+    data = new float[this.n][this.d];
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        data[i][j] = scanner.nextFloat();
+      }
+    }
   }
 
 }
