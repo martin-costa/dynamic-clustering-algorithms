d386fde32b2e6e8f90b9410fb5560da74b437f1a
martin-costa <costa.martin@outlook.com>
2022-12-09 11:11:18 +0000

    added pdf multi page plotting

--- plots.py
+++ plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 import sys
 
 # loads a long from nanoseconds as a float in seconds
@@ -30,18 +31,33 @@ def load_data_single(dataset, k, param, algo):
     return update_times, query_times, costs
 
 # create a plot for dataset and k
-def plot_data(rows):
+def plot_data(pages):
 
-    fig, axs = plt.subplots(len(rows), 3, figsize=(15, 5*len(rows)))
+    # stores the figures for each page
+    figs = []
 
-    for i in range(len(rows)):
-        plot_data_row(axs[i], rows[i][0], rows[i][1], rows[i][2], rows[i][3], rows[i][4])
+    for page in pages:
+        figs = figs + [plot_data_page(page)]
+
+    with PdfPages('test_results.pdf') as pdf:
+
+        for fig in figs:
+            pdf.savefig(fig)  # saves the current figure into a pdf page
+            plt.close()
+
+    plt.show()
+
+# create a plot for dataset and k
+def plot_data_page(page):
+
+    fig, axs = plt.subplots(len(page), 3, figsize=(18, 5*len(page)))
+
+    for i in range(len(page)):
+        plot_data_row(axs[i], page[i][0], page[i][1], page[i][2], page[i][3], page[i][4])
 
     fig.tight_layout()
 
-    plt.savefig('figures/test_results.pdf', format='pdf', bbox_inches='tight')
-
-    plt.show()
+    return fig
 
 # create a plot for dataset and k
 def plot_data_row(axs, dataset, k, params, algo, colors):
@@ -60,29 +76,40 @@ def plot_data_row(axs, dataset, k, params, algo, colors):
     # update times
     for i in range(len(params)):
         axs[0].plot(x_updates, data[i][0], label=str(params[i]) + '_' + algo, color=colors[i])
-    axs[0].set_title('Total Update Time')
+    axs[0].set_title('Total Update Time (' + dataset + ', k = ' + str(k) + ')')
     axs[0].set(xlabel='Updates', ylabel='Total Update Time (sec)')
     axs[0].legend()
 
     # query times
     for i in range(len(params)):
         axs[1].plot(x_queries, data[i][1], label=str(params[i]) + '_' + algo, color=colors[i])
-    axs[1].set_title('Total Query Time')
+    axs[1].set_title('Total Query Time (' + dataset + ', k = ' + str(k) + ')')
     axs[1].set(xlabel='Updates', ylabel='Total Query Time (sec)')
     axs[1].legend()
 
     # costs
     for i in range(len(params)):
         axs[2].plot(x_queries, data[i][2], label=str(params[i]) + '_' + algo, color=colors[i])
-    axs[2].set_title('Cost of Solution')
+    axs[2].set_title('Cost of Solution (' + dataset + ', k = ' + str(k) + ')')
     axs[2].set(xlabel='Updates', ylabel='Cost')
     axs[2].legend()
 
-    plt.suptitle('dataset = ' + dataset + ', k = ' + str(k) + ', ' + str(n) + ' updates, ' + str(q) + ' queries')
+    # plt.suptitle('dataset = ' + dataset + ', k = ' + str(k) + ', ' + str(n) + ' updates, ' + str(q) + ' queries')
 
 if __name__ == '__main__':
 
     blues = ['#20B2AA', '#00BFFF', '#0000FF']
     reds = ['#FFA500', '#EE82EE', '#FF0000']
 
-    plot_data([['census', 10, [15, 30, 60], 'BCLP', blues], ['census', 10, [250, 500, 1000], 'HK20', reds]])
+    page_k10 = [
+
+     ['census', 10, [15, 30, 60], 'BCLP', blues],
+     ['census', 10, [250, 500, 1000], 'HK20', reds],
+     ['song', 10, [15, 30, 60], 'BCLP', blues],
+     ['song', 10, [250, 500, 1000], 'HK20', reds],
+     ['kddcup', 10, [15, 30, 60], 'BCLP', blues],
+     ['kddcup', 10, [250, 500, 1000], 'HK20', reds]
+
+    ]
+
+    plot_data([page_k10])
