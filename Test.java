84e186604bd9e90cb794bbc46864ea764aefeb70
martin-costa <costa.martin@outlook.com>
2022-12-11 12:23:35 +0000

    rerun kddcup on L2 no noise

--- Test.java
+++ Test.java
@@ -44,7 +44,7 @@ public class Test {
     int queryCount = 500;
 
     // the metric to be used
-    Metric metric = new LpNorm(2, 1.0f/n);
+    Metric metric = new LpNorm(2);
 
     // create update stream
     SlidingWindow updateStream = new SlidingWindow(n, windowLength, "../data/" + dataset);
