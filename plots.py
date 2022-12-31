104f680903fc3209445467fafe5cdc0b150a08cc
martin-costa <costa.martin@outlook.com>
2022-12-31 19:55:04 +0000

    moving code

--- plots.py
+++ plots.py
@@ -109,9 +109,9 @@ def get_page(k, algos):
 
     page = [
 
-     # ['census', k, algos, [blues , reds]],
-     # ['song', k, algos, [blues , reds]],
-     # ['kddcup', k, algos, [blues , reds]],
+     # ['census', k, algos, [blues, reds]],
+     # ['song', k, algos, [blues, reds]],
+     # ['kddcup', k, algos, [blues, reds]],
 
      ['census', k, algos, [['#0000FF' , '#FF0000']]],
      ['song', k, algos, [['#0000FF' , '#FF0000']]],
@@ -134,6 +134,6 @@ if __name__ == '__main__':
     # bclp_algos = ['250_BCLP', '500_BCLP', '1000_BCLP']
     # hk20_algos = ['250_HK20', '500_HK20', '1000_HK20']
     #
-    # plot_data(get_pages([50], [bclp_algos , hk20_algos]), 'results_justification/')
+    # plot_data(get_pages([50], [bclp_algos, hk20_algos]), 'results_justification/')
 
-    plot_data(get_pages([50], [['1000_BCLP', '1000_HK20']]), 'results/')
+    plot_data(get_pages([10], [['1000_BCLP', '1000_HK20']]), 'results/')
