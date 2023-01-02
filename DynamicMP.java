6720d6e2ef0754c09ab5ec22b1f8f2e12f97a99c
martin-costa <costa.martin@outlook.com>
2023-01-02 01:57:54 +0000

    moving code

--- DynamicMP.java
+++ DynamicMP.java
@@ -337,17 +337,17 @@ public class DynamicMP extends DynamicAlgorithm {
     // add final unsampled layer
     Integer[] finalLayer = layers.getLast().keySet().toArray(new Integer[0]);
 
-    for (Integer key : finalLayer) {
-      coresetWeights.put(key, 1.0f);
-    }
+    // for (Integer key : finalLayer) {
+    //   coresetWeights.put(key, 1.0f);
+    // }
+    //
+    // System.out.println(finalLayer.length);
 
     // place the samples from each layer into the map
-    // for (int i = 0; i < samples.size(); i++) {
-    //   coresetWeights.putAll(samples.get(i));
-    //   System.out.println(samples.get(i).size());
-    // }
-
-    System.out.println(finalLayer.length);
+    for (int i = 0; i < samples.size(); i++) {
+      coresetWeights.putAll(samples.get(i));
+      System.out.println(samples.get(i).size());
+    }
 
     System.out.println(coresetWeights.size());
 
